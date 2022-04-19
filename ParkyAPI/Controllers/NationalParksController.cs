c8eb1a03763b625af3383c1b15243d011df53d80
Pratik <48886326+Shantdude@users.noreply.github.com>
2022-04-20 02:00:49 +0530

    NationalParkController added | Postman API call tested | Pratik | V1.0.4

---
+++ ParkyAPI/Controllers/NationalParksController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using ParkyAPI.Models.DTOs;
+using ParkyAPI.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] 
+    public class NationalParksController : Controller       //it will act as api controller
+    {
+        private INationalParkRepository _npr;
+        private readonly IMapper _imap;
+        public NationalParksController(INationalParkRepository npr, IMapper im)
+        {
+            _npr = npr;
+            _imap = im;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //GET
+        [HttpGet]
+        public IActionResult GetNationalPark()
+        {
+            var objs = _npr.GetNationalParks();
+
+            //but map to DTO
+            var objdto = new List<NationalParkDTO>();
+
+            foreach(var x in objs)
+            {
+                objdto.Add(_imap.Map<NationalParkDTO>(x));
+            }
+
+            return Ok(objdto);
+        }
+    }
+}
