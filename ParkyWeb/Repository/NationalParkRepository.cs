801c90e7078e89d45c150f1e6164f02f4d97c017
Pratik <48886326+Shantdude@users.noreply.github.com>
2022-04-30 22:27:45 +0530

    NationalPark and Trail repository created and inherited in Repository.cs | Pratik | V3.0.2

---
+++ ParkyWeb/Repository/NationalParkRepository.cs
@@ -0,0 +1,20 @@
+﻿using ParkyWeb.Models;
+using ParkyWeb.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Repository
+{
+    public class NationalParkRepository: Repository<NationalPark>, INationalParkRepository
+    {
+        private readonly IHttpClientFactory _icf;
+
+        public NationalParkRepository(IHttpClientFactory icf): base(icf)    //need to map Repository's _ichf to current icf
+        {
+            _icf = icf;
+        }
+    }
+}
---
+++ ParkyWeb/Repository/TrailRepository.cs
@@ -0,0 +1,19 @@
+﻿using ParkyWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ParkyWeb.Repository.IRepository;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace ParkyWeb.Repository
+{
+    public class TrailRepository: Repository<Trail>, ITrailRepository
+    {
+        private readonly IHttpClientFactory _icf;
+        public TrailRepository(IHttpClientFactory icf): base(icf)
+        {
+            _icf = icf;
+        }
+    }
+}
