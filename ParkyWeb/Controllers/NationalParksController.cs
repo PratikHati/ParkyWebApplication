9e521c95d5d6cf1ead066c675349d16858a616ec
Pratik <48886326+Shantdude@users.noreply.github.com>
2022-05-01 19:33:53 +0530

    NationalParksController created for ParkyWeb | GetAllNationalPark() backend method created | Javascript added to index page | Pratik | V3.0.3

---
+++ ParkyWeb/Controllers/NationalParksController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParkyWeb.Models;
+using ParkyWeb.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Controllers
+{
+    public class NationalParksController : Controller
+    {
+        private readonly INationalParkRepository _inpr;
+
+        public NationalParksController(INationalParkRepository inpr)
+        {
+            _inpr = inpr;
+        }
+        public IActionResult Index()
+        {
+            return View(new NationalPark() { });    //not needed 
+        }
+
+        public async Task<IActionResult> GetAllNationalPark()
+        {
+            return Json(new { data = await _inpr.GetAllAsync(SD.NationalParkApiUrl)});  //will retreive all NP objects from ParkyAPI through "Repository"
+        }
+
+
+    }
+}
