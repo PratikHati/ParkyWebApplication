13878957e4481e0a91c9ac37650d809d61f06817
Pratik Hati <pratikhati.work@gmail.com>
2022-05-04 18:44:47 +0530

    Delete functionality added at nationalpark.js using ajax | Pratik | V3.0.5(Final)

--- ParkyWeb/wwwroot/js/nationalpark.js
+++ ParkyWeb/wwwroot/js/nationalpark.js
@@ -18,10 +18,10 @@ function loadDataTable() {
                 "data": "id",
                 "render": function (data) {
                     return `<div class="text-center">
-                                <a href="/nationalParks/Upsert/${data}" class='btn btn-success text-white'
+                                <a href="/nationalParks/Upsert/${data}" class='btn btn-success text-white'      /*upsert*/
                                     style='cursor:pointer;'> <i class='far fa-edit'></i></a>
                                     &nbsp;
-                                <a onclick=Delete("/nationalParks/Delete/${data}") class='btn btn-danger text-white'
+                                <a onclick=Delete("/nationalParks/Delete/${data}") class='btn btn-danger text-white'    /*delete*/
                                     style='cursor:pointer;'> <i class='far fa-trash-alt'></i></a>
                                 </div>
                             `;
@@ -45,15 +45,15 @@ function Delete(url) {
                 url: url,
                 success: function (data) {
                     if (data.success) {
-                        toastr.success(data.message);
-                        dataTable.ajax.reload();
+                        toastr.success(data.message);   //toaster notification(success)
+                        dataTable.ajax.reload();        //reload rest objects   
                     }
                     else {
-                        toastr.error(data.message);
+                        toastr.error(data.message);     //toaster notification(error)
                     }
                 }
             });
         }
     });
 
-}
+}    
